ee7245496d7c8a097adae643494ed14feb825cb4
Eliza Regas <elizaregas@gmail.com>
2020-06-18 17:23:00 -0400

    styling buttons on hover for landing page and age verification page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -6,7 +6,11 @@ $(document).ready(function () {
 // onclick functions- DO NOT PUT THESE INSIDE DOCUMENT READY!
 // navigate from landing page to age verification page
 function toAgeVerificationPage() {
-  console.log("clicked");
   $("#landing-page").hide();
   $("#age-verification-page").show();
 };
+
+function toLandingPage() {
+  $("#landing-page").show();
+  $("#age-verification-page").hide();
+}

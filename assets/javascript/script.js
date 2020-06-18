573fcf5bf46244a3a1666782b2d70180a7110291
Eliza Regas <elizaregas@gmail.com>
2020-06-18 16:56:46 -0400

    start button links from landing page to age verification page

---
+++ assets/javascript/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+
+
+});
+
+// onclick functions- DO NOT PUT THESE INSIDE DOCUMENT READY!
+// navigate from landing page to age verification page
+function toAgeVerificationPage() {
+  console.log("clicked");
+  $("#landing-page").hide();
+  $("#age-verification-page").show();
+};

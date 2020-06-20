7a3c33837e4a8a682f9468fce893ef588022bce5
Eliza Regas <elizaregas@gmail.com>
2020-06-20 19:08:30 -0400

    displaying drinks in the DOM

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -51,6 +51,39 @@ $(document).ready(function () {
     //  $("#info-here").append(response.hosts.images);
   });
 
+  var queryInitial =
+    "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+  var searchDrink = "Artillery punch";
+  var queryURL = queryInitial + searchDrink;
+  $.ajax({
+    url: queryURL,
+    method: "GET",
+  }).then(function (response) {
+    console.log(response);
+    // in this function I will pass in the name of drink and save all data needed to be displayed.
+    $("#drink-name").append(response.drinks[0].strDrink);
+
+    $("#drink-image").append(response.drinks[0].strDrinkThumb);
+
+    for (var i = 1; i <= 15; i++) {
+      var key = "strIngredient" + i;
+      if (response.drinks[0][key] !== null) {
+        console.log(response.drinks[0][key]);
+      }
+      $("#drink-ingredients").append(response.drinks[0][key]);
+    }
+    // $("#drinks-here").append(response.drinks[0].strDrink);
+    for (var i = 1; i <= 15; i++) {
+      var measure = "strMeasure" + i;
+      if (response.drinks[0][measure] !== null) {
+        console.log(response.drinks[0][measure]);
+      }
+      $("#drink-ingredients").append(response.drinks[0][measure]);
+    }
+
+    $("#drink-instructions").append(response.drinks[0].strInstructions);
+    console.log(response.drinks[0].strInstructions);
+  })
 });
 
 // onclick functions- DO NOT PUT THESE INSIDE DOCUMENT READY!
@@ -99,3 +132,5 @@ function toResultsPage() {
   $("#booze-page").hide();
   $("#results-page").show();
 }
+
+

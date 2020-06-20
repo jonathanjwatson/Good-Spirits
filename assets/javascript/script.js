30c789cbf1d4948da7df8f6079a7e710f89d1a77
lead81 <lead81_1@yahoo.com>
2020-06-20 18:43:11 -0400

    drink

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,55 +1,77 @@
 
 $(document).ready(function () {
   var queryInitial =
-    "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
-  var searchDrink = "Artillery punch";
-  var queryURL = queryInitial + searchDrink;
-  $.ajax({
-    url: queryURL,
-    method: "GET",
-  }).then(function (response) {
-    console.log(response);
-    // in this function I will pass in the name of drink and save all data needed to be displayed.
-    $("#drink-name").append(response.drinks[0].strDrink);
+  "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+var searchDrink = "Artillery punch";
+var queryURL = queryInitial + searchDrink;
+$.ajax({
+  url: queryURL,
+  method: "GET",
+}).then(function (response) {
+  console.log(response);
+  // in this function I will pass in the name of drink and save all data needed to be displayed.
+  $("#drink-name").append(response.drinks[0].strDrink);
 
-    $("#drink-image").append(response.drinks[0].strDrinkThumb);
-    
-    
-
-    
-
-    for (var i = 1; i <= 15; i++) {
-      var key = "strIngredient" + i;
-      if (response.drinks[0][key] !== null) {
-        console.log(response.drinks[0][key]);
-       
-      }
-      $("#drink-ingredients").append(response.drinks[0][key]);
-    }
-    // $("#drinks-here").append(response.drinks[0].strDrink);
-    for (var i = 1; i <= 15; i++) {
-      var measure = "strMeasure" + i;
-      if (response.drinks[0][measure] !== null) {
-         console.log(response.drinks[0][measure]);
-      }
-      $("#drink-ingredients").append(response.drinks[0][measure]);
-    }
+  $("#drink-image").append(response.drinks[0].strDrinkThumb);
   
-    $("#drink-instructions").append(response.drinks[0].strInstructions);
-    
-    
-    
-  });
 
+  for (var i = 1; i <= 15; i++) {
+    var key = "strIngredient" + i;
+    if (response.drinks[0][key] !== null) {
+      console.log(response.drinks[0][key]);
+     
+    }
+    $("#drink-ingredients").append(response.drinks[0][key]);
+  }
+  // $("#drinks-here").append(response.drinks[0].strDrink);
+  for (var i = 1; i <= 15; i++) {
+    var measure = "strMeasure" + i;
+    if (response.drinks[0][measure] !== null) {
+       console.log(response.drinks[0][measure]);
+    }
+    $("#drink-ingredients").append(response.drinks[0][measure]);
+  }
+
+  $("#drink-instructions").append(response.drinks[0].strInstructions);
+  
+  
   
 });
+    //$("#drinks-here").append(response.drinks[0].strDrink);
+  });
+
+  var search = "eminem";
+  var settings = {
+    async: true,
+    crossDomain: true,
+    url: "https://deezerdevs-deezer.p.rapidapi.com/search?q=" + search,
+    method: "GET",
+    headers: {
+      "x-rapidapi-host": "deezerdevs-deezer.p.rapidapi.com",
+      "x-rapidapi-key": "c44498c14bmsh109c8caa20abc0ep12e719jsnc8f75e4181e0",
+    },
+  };
+  //in this anonymous function I will pass in the name of the song and save all data to be displayed.
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+    console.log(response.data[0].preview);
+
+    //  $("#info-here").append(response.hosts.images);
+  });
+
+
+
 
 // onclick functions- DO NOT PUT THESE INSIDE DOCUMENT READY!
-// navigates from landing page to age verification page
+
+// navigate from landing page to age verification page
+
+//-------------------------------------------------
 function toAgeVerificationPage() {
   $("#landing-page").hide();
   $("#age-verification-page").show();
-};
+}
+
 // redirects to landing page when user clicks "back" from any page
 function toLandingPage() {
   $("#age-verification-page").hide();
@@ -70,6 +92,7 @@ function toMusicPage() {
   $("#mood-page").hide();
   $("#music-page").show();
 }
+
 // if user is under 21, this navigates to a kids friendly page
 function toKidsPage() {
   $("#age-verification-page").hide();
